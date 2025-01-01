32cb9edddf38b8d792453d6dbc1c97b57a28ea9f
juliocesardemoraes <jcmcf2012@gmail.com>
2025-01-01 10:20:45 -0300

    feat: add toast

--- src/js/main.js
+++ src/js/main.js
@@ -11,8 +11,32 @@ const fetchData = async () => {
   console.log(data.piada.piada);
 };
 
-window.onload = () => {
+window.onload = async () => {
+  // Toastify({
+  //   text: "This is a toast",
+  //   className: "info",
+  // }).showToast();
+
   if (!localStorage.getItem("user")) {
+    window.location.href = "/login";
+  }
+
+  const bodyString = localStorage.getItem("user");
+  console.log(bodyString);
+
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/user/auth`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: bodyString,
+  });
+
+  // const responseMessage = await res.json();
+  // console.log(responseMessage);
+
+  console.log("RES", res);
+  if (res?.status !== 200) {
     window.location.href = "/login";
   }
 };
